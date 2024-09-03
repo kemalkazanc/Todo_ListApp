cb4d10694468180bf26ec906c532fb2de7323ab9
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-09-03 16:45:14 +0200

    Initial Commit

---
+++ Todo_ListApp/Todo_ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListAppApp.swift
+//  Todo_ListApp
+//
+//  Created by Kemal Kazanc on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
